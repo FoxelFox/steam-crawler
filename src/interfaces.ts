81e76868e9415e1f71b1fc131e30a9b89c9e5147
André Furchner <storkner@gmail.com>
2019-06-01 11:34:33 +0200

    crawling header.jpg

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,4 +1,4 @@
-export interface StorePage {
+export interface ItemList {
 	applist: {
 		apps: {
 			appid: number
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,79 @@
 import request = require("request");
+import {ItemList} from "./interfaces";
 import * as fs from "fs";
-import {StorePage} from "./interfaces";
+
+let items: ItemList;
 
 
-function getItems(): Promise<StorePage> {
+function getItems(): Promise<ItemList> {
 	return new Promise((resolve) => {
 		request("http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=STEAMKEY&format=json", (err, res, body) => {
-			resolve(JSON.parse(body) as StorePage);
+			resolve(JSON.parse(body) as ItemList);
 		});
 	});
 }
 
-async function run() {
-	const items =  await getItems();
 
-	for (const app of items.applist.apps) {
-		console.log(app.name);
-	}
+function fetch(path) {
+	return new Promise((resolve, reject) => {
+		request(path, {encoding: "binary"}, (err, res, body) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(body);
+			}
+		});
+	});
+}
+
+function crawl(path: string, fileName: string) {
+	return new Promise(async (resolve) => {
+
+		items =  await getItems();
+
+		if (!fs.existsSync("crawled")) {
+			fs.mkdirSync("crawled");
+		}
+
+
+		const count = items.applist.apps.length;
+		let i = 0;
+
+
+		const join = () => {
+			console.log((++i / count * 100).toFixed(2) + "%");
+
+			if (i === count) {
+				resolve();
+			}
+		};
+
+		for (const app of items.applist.apps) {
+
+			const appFileName = "crawled" + "/" + app.appid + "/" + fileName + ".jpg";
+			const appFolderName = "crawled" + "/" + app.appid.toString();
+
+			if (!fs.existsSync(appFolderName)) {
+				fs.mkdirSync(appFolderName);
+			}
+
+			if (!fs.existsSync(appFileName)) {
+				const body = await fetch(path.replace("{id}", app.appid.toString()));
+
+				fs.writeFile(appFileName, body, "binary", () => {
+					join();
+				});
+			} else {
+				join();
+			}
+		}
+	});
 }
 
 
-run().then(() => {
+crawl(
+	"https://steamcdn-a.akamaihd.net/steam/apps/{id}/header.jpg",
+	"header"
+).then(() => {
 	console.log("done");
 });
