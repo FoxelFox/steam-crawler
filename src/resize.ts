0a9b1b05bde93d43e8cb69a9e5f6dd0ce82af99f
André Furchner <storkner@gmail.com>
2019-06-02 14:47:21 +0200

    skip images that already resized

--- src/resize.ts
+++ src/resize.ts
@@ -10,10 +10,12 @@ const count = apps.length;
 async function run() {
 	for (const app of apps) {
 
-		try {
-			await sharp("crawled/" + app+ "/header.jpg").resize(64, 64).toFile("crawled/" + app + "/64.jpg")
-		} catch (e) {
-			// ignore
+		if (!fs.existsSync("crawled/" + app+ "/64.jpg")) {
+			try {
+				await sharp("crawled/" + app+ "/header.jpg").resize(64, 64).toFile("crawled/" + app + "/64.jpg")
+			} catch (e) {
+				// ignore
+			}
 		}
 
 		++i;
