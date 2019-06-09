5bf9bbc7d3ef6fb8a6ec5357320942e4d470e663
André Furchner <storkner@gmail.com>
2019-06-09 18:00:52 +0200

    using readFile Sync because out of memory

--- src/more-like-this.ts
+++ src/more-like-this.ts
@@ -12,53 +12,59 @@ async function run() {
 		const count = apps.length;
 		let i = 0;
 		for (const app of apps) {
-			fs.readFile("crawled/" + app + "/index.html", {encoding: "utf-8"}, (err, data) => {
-				if (!err) {
-					const match = data.match(/(RenderMoreLikeThisBlock.*)/g);
-					if (match) {
-						const edges = JSON.parse("[" + match[0].split("[")[1].split("]")[0] + "]");
-						for (const edge of edges) {
-							if (!directMap[app]) {
-								directMap[app] = [];
-							}
 
-							if (directMap[app].indexOf(edge) === -1) {
-								directMap[app].push(edge);
-							}
+			try {
+				const data = fs.readFileSync("crawled/" + app + "/index.html", {encoding: "utf-8"})
+				const match = data.match(/(RenderMoreLikeThisBlock.*)/g);
 
-							if (!indirectMap[edge]) {
-								indirectMap[edge] = [];
-							}
-
-
-
-							if (indirectMap[edge].indexOf(app) === -1) {
-								indirectMap[edge].push(app);
-							}
-
+				if (match) {
+					const edges = JSON.parse("[" + match[0].split("[")[1].split("]")[0] + "]");
+					for (const edge of edges) {
+						if (!directMap[app]) {
+							directMap[app] = [];
 						}
+
+						if (directMap[app].indexOf(edge) === -1) {
+							directMap[app].push(edge);
+						}
+
+						if (!indirectMap[edge]) {
+							indirectMap[edge] = [];
+						}
+
+
+
+						if (indirectMap[edge].indexOf(app) === -1) {
+							indirectMap[edge].push(app);
+						}
+
 					}
 				}
-				i++;
+			} catch (e) {
+				// ignore
+			}
 
-				if (i === count) {
 
-					for (const key in indirectMap) {
-						if (!directMap[key]) {
-							directMap[key] = indirectMap[key].slice(0, 15);
-						}
+			i++;
+
+			if (i === count) {
+
+				for (const key in indirectMap) {
+					if (!directMap[key]) {
+						directMap[key] = indirectMap[key].slice(0, 15);
 					}
-
-
-
-					for (const key in directMap) {
-						for (const edge of directMap[key]) {
-							csv += [key, edge, 1].join("|") + "\n";
-						}
-					}
-					resolve(csv);
 				}
-			});
+
+
+
+				for (const key in directMap) {
+					for (const edge of directMap[key]) {
+						csv += [key, edge, 1].join("|") + "\n";
+					}
+				}
+				resolve(csv);
+			}
+
 		}
 	});
 
