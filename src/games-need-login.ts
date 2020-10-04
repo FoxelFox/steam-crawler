09b64f183640d39845ff1c060a21d510e83e10e2
André Furchner <storkner@gmail.com>
2020-10-04 16:31:32 +0200

    new html list scripts

---
+++ src/games-need-login.ts
@@ -0,0 +1,80 @@
+import * as fs from "fs";
+
+const search = "You must login to see this content."
+
+async function run() {
+    return new Promise((resolve) => {
+        const apps = fs.readdirSync("crawled");
+
+        const items = []
+        let i = 0
+        for (const app of apps) {
+            try {
+
+                if (fs.statSync("crawled/" + app + "/header.jpg")["size"] > 1000) {
+                    const data = fs.readFileSync("crawled/" + app + "/index.html", {encoding: "utf-8"})
+
+                    if (data.indexOf(search) != -1) {
+                        items.push(app)
+                        i++;
+                    }
+                }
+
+            } catch {
+                // ignore
+            }
+
+
+
+            if (i > 100) {
+                //break;
+            }
+
+        }
+
+        resolve(items);
+    });
+}
+
+run().then((items: string[]) => {
+
+    const start = `
+        <!DOCTYPE html>
+        <html lang="en" >
+        
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width,initial-scale=1">
+                <meta name="theme-color" content="#161616">
+                <title>Steam Graph</title>
+                <style>
+                    /* remove the border */
+                    body {
+                        border: 0;
+                        background-color: #000;
+                        margin: 0;
+                    }
+                </style>
+            </head>
+        
+            <body>
+    `
+
+    const end = `
+            </body>
+        </html>    
+    `
+
+    let list = "";
+
+
+
+    for (const item of items) {
+        list += `
+<a href="https://store.steampowered.com/app/${item}">
+    <img loading="lazy" width="460" height="215" src="https://steamcdn-a.akamaihd.net/steam/apps/${item}/header.jpg" onerror="this.onerror=null;this.src='crawled/${item}/header.jpg';" >
+</a>`
+    }
+
+    fs.writeFileSync("games-need-login.html", start + list + end )
+})
---
+++ src/html-list-all.ts
@@ -0,0 +1,64 @@
+import * as fs from "fs";
+
+async function run() {
+    return new Promise((resolve) => {
+        const apps = fs.readdirSync("crawled");
+
+        const items = []
+        for (const app of apps) {
+            try {
+
+                if (fs.statSync("crawled/" + app + "/header.jpg")["size"] > 1000) {
+                    items.push(app)
+                }
+            } catch {
+                // ignore
+            }
+        }
+
+        resolve(items);
+    });
+}
+
+run().then((items: string[]) => {
+
+    const start = `
+        <!DOCTYPE html>
+        <html lang="en" >
+        
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width,initial-scale=1">
+                <meta name="theme-color" content="#161616">
+                <title>Steam Graph</title>
+                <style>
+                    /* remove the border */
+                    body {
+                        border: 0;
+                        background-color: #000;
+                        margin: 0;
+                    }
+                </style>
+            </head>
+        
+            <body>
+    `
+
+    const end = `
+            </body>
+        </html>    
+    `
+
+    let list = "";
+
+
+
+    for (const item of items) {
+        list += `
+<a href="https://store.steampowered.com/app/${item}">
+    <img loading="lazy" width="460" height="215" src="https://steamcdn-a.akamaihd.net/steam/apps/${item}/header.jpg">
+</a>`
+    }
+
+    fs.writeFileSync("games.html", start + list + end )
+})
